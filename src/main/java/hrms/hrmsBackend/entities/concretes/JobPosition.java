3404531707d857363b9e803e7745ecfe18c14082
cagllan <caglanatay@windowslive.com>
2021-05-10 20:20:46 +0300

    JobPosition class oluşturuldu
    entity class ı oluşturuldu

---
+++ src/main/java/hrms/hrmsBackend/entities/concretes/JobPosition.java
@@ -0,0 +1,31 @@
+package hrms.hrmsBackend.entities.concretes;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name="jobpositions")
+public class JobPosition {
+	
+	@Id
+	@GeneratedValue
+	@Column(name="id")
+	private int id;
+	
+	@Column(name="jobposition_name")
+	private String jobPositionName;	
+	
+	public JobPosition(int id, String jobPositionName) {
+		super();
+		this.id = id;
+		this.jobPositionName = jobPositionName;
+	}
+	
+	
+}

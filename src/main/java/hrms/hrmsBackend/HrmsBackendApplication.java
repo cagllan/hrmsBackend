e47e5d876d77687d98e2100d7d1943c2c607f9ac
cagllan <caglanatay@windowslive.com>
2021-05-10 20:05:18 +0300

    proje oluşturuldu

---
+++ src/main/java/hrms/hrmsBackend/HrmsBackendApplication.java
@@ -0,0 +1,13 @@
+package hrms.hrmsBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrmsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrmsBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hrms/hrmsBackend/HrmsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package hrms.hrmsBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrmsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

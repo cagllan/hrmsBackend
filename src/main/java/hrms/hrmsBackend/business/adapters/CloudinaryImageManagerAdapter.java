22fb136334f7537d5c8ac3e38b5af34643912189
cagllan <caglanatay@windowslive.com>
2021-06-11 16:21:23 +0300

    cloudinary api key

--- src/main/java/hrms/hrmsBackend/business/adapters/CloudinaryImageManagerAdapter.java
+++ src/main/java/hrms/hrmsBackend/business/adapters/CloudinaryImageManagerAdapter.java
@@ -9,6 +9,7 @@ import org.springframework.web.multipart.MultipartFile;
 import com.cloudinary.Cloudinary;
 import com.cloudinary.utils.ObjectUtils;
 
+import hrms.hrmsBackend.business.constants.CloudinaryApiKey;
 import hrms.hrmsBackend.core.utilites.results.DataResult;
 import hrms.hrmsBackend.core.utilites.results.ErrorDataResult;
 import hrms.hrmsBackend.core.utilites.results.SuccessDataResult;
@@ -21,7 +22,10 @@ public class CloudinaryImageManagerAdapter implements CloudinaryImageService{
 	
 	public CloudinaryImageManagerAdapter() {
 		
-		this.cloudinary = new Cloudinary();
+		this.cloudinary = new Cloudinary(ObjectUtils.asMap(
+				"cloud_name", CloudinaryApiKey.cloud_name,
+				"api_key", CloudinaryApiKey.api_key,
+				"api_secret", CloudinaryApiKey.api_secret));
 	}
 
 
---
+++ src/main/java/hrms/hrmsBackend/business/constants/CloudinaryApiKey_sample.java
@@ -0,0 +1,7 @@
+package hrms.hrmsBackend.business.constants;
+
+public class CloudinaryApiKey_sample {
+	public static String cloud_name = "Your_Cloud_Name";
+	public static String api_key = "Your_Api_Key";
+	public static String api_secret = "Your_Api_Secret";
+}
